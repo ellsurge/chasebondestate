67c044305c96780ff4c7bc509ada03f7b20aef7c
bendigiorgio <bendigiorgio@gmail.com>
2023-06-16 13:23:34 +0900

    studio and google maps selection

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,20 +1,27 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
 
-import {schemaTypes} from './src/schemas'
+import { schemaTypes } from "./src/schemas";
+import { googleMapsInput } from "@sanity/google-maps-input";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const mapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!;
 
 export default defineConfig({
-  basePath: '/studio', // <-- important that `basePath` matches the route you're mounting your studio from, it applies to both `/pages` and `/app`
+  basePath: "/studio",
 
   projectId,
   dataset,
 
-  plugins: [deskTool()],
+  plugins: [
+    deskTool(),
+    googleMapsInput({
+      apiKey: mapsApiKey,
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
